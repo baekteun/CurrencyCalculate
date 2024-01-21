85a1a75db0325950008c76b6ec588081f2e56883
baegteun <baegteun@gmail.com>
2024-01-21 15:40:51 +0900

    :art: :: 기본 폴더구조 설정

--- Currency/Feature/CurrencyCalculatorFeature/CurrencyCalculatorViewController.swift
+++ Currency/Feature/CurrencyCalculatorFeature/CurrencyCalculatorViewController.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-final class ViewController: UIViewController {
+final class CurrencyCalculatorViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }

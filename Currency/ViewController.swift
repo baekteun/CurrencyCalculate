a7c0526108111736e751b58464f898f45a901320
baegteun <baegteun@gmail.com>
2024-01-21 15:29:40 +0900

    :sparkles: :: 스토리보드 제거 세팅

--- Currency/ViewController.swift
+++ Currency/ViewController.swift
@@ -1,19 +1,7 @@
-//
-//  ViewController.swift
-//  Currency
-//
-//  Created by 최형우 on 1/21/24.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
